20c345fa47e3ac908647eed8bf7e7ee9ee376c75
filin-from-2042 <filin2042@gmail.com>
2013-11-04 20:10:38 +0300

    Update mysql.base.sql
    tbl_tag table

--- protected/data/mysql.base.sql
+++ protected/data/mysql.base.sql
@@ -1,4 +1,9 @@
 CREATE DATABASE IF NOT EXISTS `Lynxcms`
         CHARSET=utf-8 COLLATE=utf-8_unicode_ci;
-
+        
+CREATE TABLE `tbl_tag`(
+        `tag_id` INT NOT NULL AUTO_INCREMENT,
+        `tag_name` VARCHAR(150) NOT NULL
+        CONSTRAINT `pk_tag_id` PRIMARY KEY (tag_id)
+);
 
